102ae1a5d987bdd36ad9a8d184a810e58160f5ac
haiminhpham <phamhaiminh2003@gmail.com>
2026-04-02 15:53:59 +1000

    refer to background pic correctly for production

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,10 +1,11 @@
+import Background from '../public/Background.png';
+
 const Layout = ({ children }) => {
   return (
     <div
       className="min-h-screen bg-cover bg-center text-white"
-      style={{ backgroundImage: "url('/Background.png')" }}
+      style={{ backgroundImage: `url(${Background})` }}
     >
-      {/* overlay */}
       <div className="min-h-screen bg-black/60">
         {children}
       </div>
